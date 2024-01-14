5d36ec7353ffa6e294e324418fb09713107bc585
milo <pannatorn30@gmail.com>
2024-01-14 11:30:36 +0700

    Add project files.

---
+++ WebApplication1/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "The student name field is empty.")] //ห้ามเป็นค่าว่าง
+        [DisplayName("Student Name")]
+        public string Name { get; set; }
+
+        [DisplayName("Test Score")]
+        [Range(0,100,ErrorMessage = "The test scores must be in the range 0-100.")]
+        public int Score { get; set; }
+    }
+}
